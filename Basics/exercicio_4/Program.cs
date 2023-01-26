94ca94f9fee77621a547f945a185a431f85cdb6f
Carlos <carloskitazawa@hotmail.com>
2023-01-26 12:42:37 -0300

    primeiro commit

---
+++ Basics/exercicio_4/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(-1 + 4 * 6);
+Console.WriteLine((35+5) % 7);
+Console.WriteLine(14 + -4 * 6/11);
+Console.WriteLine(2+15/6*1-7%2);
