45d8df4bd9d053380957fecba9e62c3934cab8e5
Carlos <carloskitazawa@hotmail.com>
2023-02-01 11:00:30 -0300

    commit

---
+++ Basics/exercicio_5/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+public class exercicio_5{
+    public static void Main(string[] args){
+        int numA, numB;
+
+        Console.Write("\nDigite o primeiro numero: ");
+        numA = int.Parse(Console.ReadLine());
+
+        Console.Write("\nDigite o segundo numero: ");
+        numB = int.Parse(Console.ReadLine());
+
+        Console.Write("\nO numeros trocados foram foram: \nA: "+numB);
+        Console.Write("\nB: "+numA);
+    }
+}
